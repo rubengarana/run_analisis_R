b5bc6508d0800497ac9245d54ef35894dcc54b91
Rubén González Arana <71521779+rubengarana@users.noreply.github.com>
2020-11-15 10:03:13 -0600

    Update Run_analysis2.R

--- Run_analysis2.R
+++ Run_analysis2.R
@@ -1,11 +1,11 @@
 library(plyr)
 library(dplyr)
-
+#Export the files in txt format of the trainings, tests, activity labels and the subject who performed the activity.
 act <- read.table("./activity_labels.txt")
 fea <- read.table("./features.txt")
 t <- list.files("./train/",pattern = ".txt")
 te <- list.files("./test/",pattern = ".txt")
-
+#combines the activity files, variables and subjects in a single data frame for both training and testing.
 t <- sapply(paste0("./train/",t), read.table)
 te <- sapply(paste0("./test/",te), read.table)
 
@@ -24,14 +24,16 @@ for (y in seq(length(te))) {
                 t2 <- te[[y]]
         }        
 }
-
+#unifies training and tests in a single data frame.
 total <- rbind(t1,t2)
+#change the name of each column to a more descriptive one: subjects, activities and the characteristics according to the features.txt file
 names(total)[1] <- "subject" ; names(total)[ncol(total)] <- "activity"
 names(total)[2:562] <- as.character(fea$V2)
 total$activity <- factor(total$activity,labels = as.character(act$V2))
+#Only the columns containing the words mean and standard deviation are extracted, as well as activity and subject.
+#the data frame is grouped by the activity and subject variables to generate a data frame grouped by these variables.
 media_std <- select(total,contains("mean")|contains("std")|contains("activity")|contains("subject"))%>%
         group_by(activity,subject)%>%summarize_all(mean)  
 
 media_std
-
 
